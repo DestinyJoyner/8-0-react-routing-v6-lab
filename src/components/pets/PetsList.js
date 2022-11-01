aa461379a85766b2be282092e571be185d5225f4
DestinyJoyner <destinyjoyner@pursuit.org>
2022-11-01 16:31:04 -0400

    pets separated from dogs and cats when click on pets nav bar

--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -1,8 +1,14 @@
 import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
+import { useParams } from "react-router-dom";
 
-export const PetsList = ({ pets }) => {
+export const PetsList = ({ pets}) => {
+  let {type} = useParams()
+  console.log(type)
+  // type is 'undefined' when just /pets so for all pets need OR undefined conditional
+  
+  // filters out pets into a dog array and cat array
   const [cats, dogs] = pets.reduce(
     (acc, pet) => {
       const position = pet.kind === "Cat" ? 0 : 1;
@@ -12,19 +18,23 @@ export const PetsList = ({ pets }) => {
     [[], []]
   );
 
+  
+
   return (
     <section className="pets-wrapper">
-      <PetsListNav cats={cats} dogs={dogs} />
+      <PetsListNav cats = {cats} dogs = {dogs}  />
       <section className="pets-list">
+        
         {/* All cats section */}
-        {cats.map((cat) => (
-          <Pet key={cat.id} kind="cat" pet={cat} />
-        ))}
+        {type === `cats` || type === undefined ? cats.map((cat) => (
+          <Pet key={cat.id} kind= {"cat"} pet={cat} />
+        )) : null}
 
         {/* All dogs section */}
-        {dogs.map((dog) => (
-          <Pet key={dog.id} kind="dog" pet={dog} />
-        ))}
+        {type === `dogs` || type === undefined ? dogs.map((dog) => (
+          <Pet key={dog.id} kind= {"dog"} pet={dog} />
+        )) : null}
+
       </section>
     </section>
   );
